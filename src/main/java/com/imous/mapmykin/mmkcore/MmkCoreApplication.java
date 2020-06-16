0569bb8a4c420a151614b8c7750e591fd2d5db44
Imz <imtiyaz88@gmail.com>
2020-06-17 00:38:18 +0530

    init

---
+++ src/main/java/com/imous/mapmykin/mmkcore/MmkCoreApplication.java
@@ -0,0 +1,13 @@
+package com.imous.mapmykin.mmkcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MmkCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MmkCoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/imous/mapmykin/mmkcore/MmkCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imous.mapmykin.mmkcore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MmkCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
